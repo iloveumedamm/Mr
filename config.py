38e8ffe785e964472c498966be69cb38857f3370
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2023-12-14 20:30:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH", "294a7bf4488b21609436de1cdd05c316")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    TELEGRAM_API = os.environ["TELEGRAM_API", "25603034"]
+    OWNER = os.environ.get("OWNER", "5791145987")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "Anonymous190404")
     SESSION_NAME = os.environ.get("SESSION_NAME", "merge-bot")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "streaamdb")
+    PASSWORD = os.environ.get("PASSWORD", "19040456")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://xajay10997:Xr1p2CNHjIJLrHl8@cluster0.bjsdwy9.mongodb.net/?retryWrites=true&w=majority")
+    LOGCHANNEL = os.environ.get("LOGCHANNEL", "-1001953206885")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = True
